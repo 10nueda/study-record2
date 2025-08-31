dfe263dde5b6a15cf483eaeadcc46d3f87b6a98b
nueda10n <nueda10n@gmail.com>
2025-09-01 05:53:59 +0900

    first commit

---
+++ src/__tests__/AppComponent.spec.tsx
@@ -0,0 +1,10 @@
+import App from "../App";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+
+describe("title", () => {
+  it("should render title", () => {
+    render(<App />);
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
+  });
+});
